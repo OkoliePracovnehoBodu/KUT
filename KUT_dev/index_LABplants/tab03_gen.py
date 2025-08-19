c63d6564ec6f8642a4caef0a65de108e501e8115
PracovnyBod <marian.tarnik@stuba.sk>
2025-08-19 16:12:58 +0200

    Add initial LaTeX files and scripts for idx01

---
+++ KUT_dev/index_LABplants/tab03_gen.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+
+
+# -----------------------------------------------------------------------------
+output_file = 'tab03_rows.tex'
+
+selected_KUT_items = [
+    'KUT013', 
+]
+
+# -----------------------------------------------------------------------------
+
+
+import os
+
+directory = "../../KUT_items"
+items = os.listdir(directory)
+
+
+data = []
+
+for item in items:
+    item_texfile = directory + '/' + item + '/TeX/' + item + '.tex'
+
+    with open(item_texfile, 'r', encoding='utf-8') as file:
+        content = file.read()
+        start_marker = '\\begin{flushleft}'
+        end_marker = '\\end{flushleft}'
+        start_index = content.find(start_marker)
+        end_index = content.find(end_marker)
+        if start_index != -1 and end_index != -1:
+            extracted_string = content[start_index + len(start_marker):end_index].strip()
+            data.append([item, extracted_string])
+        else:
+            data.append([item, " "])
+
+
+with open(output_file, 'w', encoding='utf-8') as file:
+    for item, extracted_string in data:
+
+        if item in selected_KUT_items:
+
+            extracted_string = extracted_string.replace('\\', '')
+
+            print(item, extracted_string)
+
+            file.write(f"\\href{{run:../../KUT_items/{item}/TeX/{item}.pdf}}{{{item}}} & {extracted_string} \\\\ \\addlinespace[3pt]  \n")
+
+
+
